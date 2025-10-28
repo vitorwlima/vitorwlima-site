5d2b8fa0dd36c8ef4181362869aa1faea063a33f
Vitor Windberg <vitorwlima13@gmail.com>
2025-10-27 22:37:38 -0300

    add homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,7 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(nunito.variable, "min-h-screen font-nunito antialiased")}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,54 @@
+import DottedGlowBackground from "@/components/ui/dotted-glow-background";
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <h1>Vitor Lima</h1>
+    <div className="relative h-screen bg-zinc-950 text-zinc-50">
+      <div className="flex flex-col items-center justify-center absolute top-0 left-0 w-full h-full z-10 bg-black/50">
+        <h1 className="text-lg">
+          Hi, I&apos;m{" "}
+          <strong className="text-4xl font-semibold">Vitor Lima</strong>
+        </h1>
+
+        <p className="mt-2">
+          Software engineer, currently building{" "}
+          <Link
+            href="https://matrices.ai"
+            target="_blank"
+            className="text-cyan-400 underline"
+          >
+            Matrices
+          </Link>
+          .
+        </p>
+
+        <p className="mt-1">
+          I am starting to write more often, check out my{" "}
+          <Link
+            href="https://vitorlima.com/blog"
+            target="_blank"
+            className="text-cyan-400 underline"
+          >
+            blog here
+          </Link>
+          .
+        </p>
+      </div>
+
+      <DottedGlowBackground
+        className="pointer-events-none mask-radial-to-90% mask-radial-at-center"
+        opacity={1}
+        gap={10}
+        radius={1.6}
+        colorLightVar="--color-neutral-500"
+        glowColorLightVar="--color-neutral-600"
+        colorDarkVar="--color-neutral-500"
+        glowColorDarkVar="--color-sky-800"
+        backgroundOpacity={0}
+        speedMin={0.3}
+        speedMax={1.6}
+        speedScale={1}
+      />
     </div>
   );
 };
