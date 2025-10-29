f96ccab1c4252f84d9af2016f0d06f8cd2f476c1
Vitor Windberg <vitorwlima13@gmail.com>
2025-10-28 21:01:50 -0300

    remove comments

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -14,14 +14,12 @@ const BlogLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <main className="flex relative">
-      {/* Desktop Sidebar */}
       <aside className="hidden md:flex max-w-[280px] w-full p-4 lg:p-8 flex-col gap-1 sticky h-fit top-0">
         {allPosts.map((post) => {
           return <PostLink key={post.slug} post={post} />;
         })}
       </aside>
 
-      {/* Mobile Drawer */}
       <SidebarDrawer posts={allPosts} />
 
       {children}
